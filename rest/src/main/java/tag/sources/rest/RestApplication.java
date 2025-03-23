59ea60195b852e5f431b72a87a22faa0d74a7ce4
mariyamax <89139747+mariyamax@users.noreply.github.com>
2025-03-23 11:07:02 +0300

    first commit

--- rest/src/main/java/tag/sources/rest/RestApplication.java
+++ rest/src/main/java/tag/sources/rest/RestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class restApplication {
+public class RestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(restApplication.class, args);
